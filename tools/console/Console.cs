abce8244cb38dc9849b560bd99ae991e34daf7f8
Rolandas Barysas <r.b@riseup.net>
2014-09-03 23:35:09 +0300

    code formatting

--- tools/console/Console.cs
+++ tools/console/Console.cs
@@ -3,106 +3,106 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class Console : MonoBehaviour {
-	public GUISkin guiskin;
-	private Rect windowRect = new Rect(Screen.width / 2 - 300, Screen.height / 2 - 200, 600, 400);
-	private bool consoleEnabled = false; 
-	public bool printInUnityConsole = false;		// Show output also in unity console
+  public GUISkin guiskin;
+  private Rect windowRect = new Rect(Screen.width / 2 - 300, Screen.height / 2 - 200, 600, 400);
+  private bool consoleEnabled = false; 
+  public bool printInUnityConsole = false;    // Show output also in unity console
 
-	[HideInInspector]
-	private string consoleInput = "";
-	private List<string> consoleMessages = new List<string>();
-	private Vector2 scrollableArea = Vector2.zero;
+  [HideInInspector]
+  private string consoleInput = "";
+  private List<string> consoleMessages = new List<string>();
+  private Vector2 scrollableArea = Vector2.zero;
 
-	void OnGUI() {
-		GUI.skin = guiskin;
+  void OnGUI() {
+    GUI.skin = guiskin;
 
-		if (consoleEnabled) {
-			windowRect = GUI.Window (1, windowRect, ConsoleWindow, "");
-			GUI.FocusControl("ConsoleInputControl");
-		}
+    if (consoleEnabled) {
+      windowRect = GUI.Window (1, windowRect, ConsoleWindow, "");
+      GUI.FocusControl("ConsoleInputControl");
+    }
 
-		Event e = Event.current;
-		if (e.keyCode == KeyCode.BackQuote && e.type == EventType.KeyUp) {
-			ToggleConsole();
-		}
+    Event e = Event.current;
+    if (e.keyCode == KeyCode.BackQuote && e.type == EventType.KeyUp) {
+      ToggleConsole();
+    }
 
-		if (e.keyCode == KeyCode.Return) {
-			ExecuteCommand(consoleInput);
-		}
-	}
+    if (e.keyCode == KeyCode.Return) {
+      ExecuteCommand(consoleInput);
+    }
+  }
 
-	void ToggleConsole() {
-		consoleEnabled = !consoleEnabled;
-	}
+  void ToggleConsole() {
+    consoleEnabled = !consoleEnabled;
+  }
 
-	void ExecuteCommand(string _command) {
-		if (consoleInput != "") {
-			Message("$> "+consoleInput);
-			List<string> response = new List<string>();
-			response = gameObject.GetComponent<ConsoleCommands>().Execute(_command);
-			response.ForEach(delegate(string _message) {
-				Message(_message);
-			});
-			consoleInput = "";
-			scrollableArea.y = Mathf.Infinity;
-		}
-	}
+  void ExecuteCommand(string _command) {
+    if (consoleInput != "") {
+      Message("$> "+consoleInput);
+      List<string> response = new List<string>();
+      response = gameObject.GetComponent<ConsoleCommands>().Execute(_command);
+      response.ForEach(delegate(string _message) {
+        Message(_message);
+      });
+      consoleInput = "";
+      scrollableArea.y = Mathf.Infinity;
+    }
+  }
 
-	void Update() {
-		if (consoleInput == "`") {
-			consoleInput = "";
-		}
+  void Update() {
+    if (consoleInput == "`") {
+      consoleInput = "";
+    }
 
-		if (consoleInput.IndexOf("`") != -1) {
-			consoleInput = consoleInput.TrimEnd('`');
-		}
-	}
+    if (consoleInput.IndexOf("`") != -1) {
+      consoleInput = consoleInput.TrimEnd('`');
+    }
+  }
 
-	void ConsoleWindow(int windowID) {
-		GUI.DragWindow(new Rect(0, 0, 600, 40));
+  void ConsoleWindow(int windowID) {
+    GUI.DragWindow(new Rect(0, 0, 600, 40));
 
-		GUI.Label(new Rect(20, 10, 200, 30), "Console");
+    GUI.Label(new Rect(20, 10, 200, 30), "Console");
 
-		/* Console messages */
-		scrollableArea = GUI.BeginScrollView(new Rect(20, 40, 560, 300), scrollableArea, new Rect(0, 0, 400, (18 * consoleMessages.Count)+10 ));
-		DrawMessages();
-		GUI.EndScrollView();
+    /* Console messages */
+    scrollableArea = GUI.BeginScrollView(new Rect(20, 40, 560, 300), scrollableArea, new Rect(0, 0, 400, (18 * consoleMessages.Count)+10 ));
+    DrawMessages();
+    GUI.EndScrollView();
 
-		GUI.SetNextControlName("ConsoleInputControl");
-		consoleInput = GUI.TextField(new Rect(20, 350, 460, 30), consoleInput, 50);
-		GUI.Label(new Rect(30, 349, 40, 30), "$>", "ConsoleRibbon");
-		if (GUI.Button(new Rect(490, 350, 90, 30), "Send", "ButtonStyle")) {
-			ExecuteCommand(consoleInput);
-		}
-	}
+    GUI.SetNextControlName("ConsoleInputControl");
+    consoleInput = GUI.TextField(new Rect(20, 350, 460, 30), consoleInput, 50);
+    GUI.Label(new Rect(30, 349, 40, 30), "$>", "ConsoleRibbon");
+    if (GUI.Button(new Rect(490, 350, 90, 30), "Send", "ButtonStyle")) {
+      ExecuteCommand(consoleInput);
+    }
+  }
 
-	/**	@desc: Draws all messages from messages list
-	 *	@params: none
-	 */
-	void DrawMessages() {
-		foreach (string _message in consoleMessages) {
-			GUILayout.BeginHorizontal();
-			if (_message[0] == '$') {
-				GUI.color = Color.gray;
-			}
-			GUILayout.Label(_message, "ConsoleMessage");
-			GUI.color = Color.white;
-			GUILayout.EndHorizontal();
-		}
-	}
+  /** @desc: Draws all messages from messages list
+   *  @params: none
+   */
+  void DrawMessages() {
+    foreach (string _message in consoleMessages) {
+      GUILayout.BeginHorizontal();
+      if (_message[0] == '$') {
+        GUI.color = Color.gray;
+      }
+      GUILayout.Label(_message, "ConsoleMessage");
+      GUI.color = Color.white;
+      GUILayout.EndHorizontal();
+    }
+  }
 
-	/**	@desc: Creates a standard message in console
-	 *	@params: {string} message text
-	 */
-	void Message(string _text) {
-		consoleMessages.Add(_text);
+  /** @desc: Creates a standard message in console
+   *  @params: {string} message text
+   */
+  void Message(string _text) {
+    consoleMessages.Add(_text);
 
-		if (printInUnityConsole) {
-			print(_text);
-		}
-	}
+    if (printInUnityConsole) {
+      print(_text);
+    }
+  }
 
-	void Start() {
-		Message("Type `help` for command list.");
-	}
+  void Start() {
+    Message("Type `help` for command list.");
+  }
 }
--- tools/console/ConsoleCommands.cs
+++ tools/console/ConsoleCommands.cs
@@ -3,76 +3,76 @@ using System.Collections;
 using System.Collections.Generic;
 
 public class ConsoleCommands : MonoBehaviour {
-	public List<string> response = new List<string>();
-	
-	public List<string> Execute(string _command) {
-		string[] words = _command.Split(' '); 
-		string command = words[0];
+  public List<string> response = new List<string>();
+  
+  public List<string> Execute(string _command) {
+    string[] words = _command.Split(' '); 
+    string command = words[0];
 
-		// Clear list of old messages
-		response.Clear();
+    // Clear list of old messages
+    response.Clear();
 
-		try {
-			switch(command) {
-				case "help":
-					return Help();
-				case "example":
-					return Example();
-				case "exampleArgs":
-					return ExampleArgs();
-				default:
-					response.Add(command+": unrecognized command");
-					return response;
-			}
-		} catch(System.IndexOutOfRangeException) {
-			response.Add("Invalid arguments (check help)");
-			return response;
-		}
-	}
+    try {
+      switch(command) {
+        case "help":
+          return Help();
+        case "example":
+          return Example();
+        case "exampleArgs":
+          return ExampleArgs();
+        default:
+          response.Add(command+": unrecognized command");
+          return response;
+      }
+    } catch(System.IndexOutOfRangeException) {
+      response.Add("Invalid arguments (check help)");
+      return response;
+    }
+  }
 
-	/**	Lists all available commands
-	 *	@return: List {string} All available commands
-	 */
-	public List<string> Help() {
-		response.Add("Available commands:");
-		response.Add("- help [shows this menu]");
-		response.Add("- example [shows example command]");
-		response.Add("- exampleArgs {number} [shows example command using arguments]");
-		
-		return response;
-	}
+  /** Lists all available commands
+   *  @return: List {string} All available commands
+   */
+  public List<string> Help() {
+    response.Add("Available commands:");
+    response.Add("- help [shows this menu]");
+    response.Add("- example [shows example command]");
+    response.Add("- exampleArgs {number} [shows example command using arguments]");
+    
+    return response;
+  }
 
-	/**	Example command with arguments
-	 *	@params: {string} [0, 1]
-	 */
-	public List<string> ExampleArgs(string arg) {
-		try {
-			switch (arg) {
-				case "1":
-					response.Add("Example command with argument 1.");
-					break;
-				case "0":
-					response.Add("Example command with argument 0.");
-					break;
-				default:
-					response.Add("Invalid argument (should be number 1 or 0)");
-					break;
-			}
+  /** Example command with arguments
+   *  @params: {string} [0, 1]
+   */
+  public List<string> ExampleArgs(string arg) {
+    try {
+      switch (arg) {
+        case "1":
+          response.Add("Example command with argument 1.");
+          break;
+        case "0":
+          response.Add("Example command with argument 0.");
+          break;
+        default:
+          response.Add("Invalid argument (should be number 1 or 0)");
+          break;
+      }
 
-			return response;
-		} catch (System.IndexOutOfRangeException) {
-			response.Add("Invalid argument (should be only 1 or 0)");
-			return response;
-		}
-	}
+      return response;
+    } catch (System.IndexOutOfRangeException) {
+      response.Add("Invalid argument (should be only 1 or 0)");
+      return response;
+    }
+  }
 
-	// Example command
-	public List<string> Example() {
-		// Do your stuff here..
+  // Example command
+  public List<string> Example() {
+    // Do your stuff here..
 
-		// How to print text to console
-		response.Add("This is example command.");
+    // How to print text to console
+    response.Add("This is example command.");
 
-		return response;
-	}
+    return response;
+  }
 }
